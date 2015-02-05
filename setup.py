14708706a77db00f2229d38409739a0b92b2c558
Julien Pagès <j.parkouss@gmail.com>
2015-02-05 23:01:52 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+from setuptools import setup
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def read(*parts):
+    return open(os.path.join(THIS_DIR, *parts)).read()
+
+def version():
+    return re.findall("__version__ = '([\d\.]+)'",
+                      read('subseek', '__init__.py'))[0]
+
+setup(name='subseek',
+      version=version(),
+      author=u"Julien Pagès",
+      author_email="j.parkouss@gmail.com",
+      description=("A command line tool to download subtitles for your "
+                   "movies."),
+      keywords="subtitles movies srt opensubtitles",
+      license="GPLv3",
+      packages=['subseek', 'subseek.sources'],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Topic :: Utilities",
+                   "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                   "Environment :: Console",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 2.7"],
+      install_requires=["requests"],
+      entry_points={
+        'console_scripts': ['subseek = subseek.main:main']
+      },
+)
